4a630e3cc5d6487813f15a99e7520b8903652efc
Marcelly Oliveira <160605874+Rayoliveir@users.noreply.github.com>
2026-02-05 12:53:02 -0300

    Add files via upload

---
+++ aula1/Program.cs
@@ -0,0 +1,39 @@
+﻿class Principal {
+    static void Main(string[] args) {
+        double Divida = 5000;
+
+        Console.Write("Digite seu nome: ");
+        string Nome = Console.ReadLine() !;
+
+        do
+        {
+        valorpagar:
+            Console.WriteLine("Digite o valor que você quer pagar da dívida: ");
+            double ValorPago = Convert.ToDouble(Console.ReadLine());
+            if (ValorPago <= Divida)
+            {
+                Divida -= ValorPago;
+                Console.WriteLine($"\nValor pago: {ValorPago:C}. \nDívida restante: {Divida:C}.");
+                if (Divida > 0) {
+                    Console.WriteLine("Deseja pagar mais alguma coisa? (s/n)");
+                    string opcao = Console.ReadLine();
+
+                    switch (opcao.ToLower()) {
+                        case "s":
+                            goto valorpagar;
+                        default:
+                            Console.WriteLine($"Obrigado, {Nome}. Até a próxima!");
+                            break;
+                    }
+                }
+            }
+            else if (ValorPago > Divida) {
+                Console.WriteLine("O valor pago é maior do que a dívida. Por favor, tente novamente digitando um valor válido.");
+            }
+            else {
+                Console.WriteLine("Valor inválido. Por favor, tente novamente.");
+            }
+        } while (Divida > 0);
+
+    }
+}
