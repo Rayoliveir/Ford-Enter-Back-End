e8ad2aab99833659e75ce681d967e98ae0900a4c
Marcelly Oliveira <160605874+Rayoliveir@users.noreply.github.com>
2026-03-09 05:18:02 -1200

    Add files via upload

---
+++ OxenteBurguer/OxenteBurguer/Models/Lanche.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OxenteBurguer.Models {
+    public class Lanche : Produto {
+        public string PontoCarne { get; set; } = string.Empty;
+        public List<string> Adicionais { get; set; } = new List<string>();
+    }
+}
